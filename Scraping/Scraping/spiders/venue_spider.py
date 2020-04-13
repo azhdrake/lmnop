14bc2de6418527f5d34a7c00ab0fee19a79f92af
B33 <my@email.com>
2020-04-13 15:56:15 -0500

    Comment and clean code

--- Scraping/Scraping/spiders/venue_spider.py
+++ Scraping/Scraping/spiders/venue_spider.py
@@ -16,8 +16,9 @@ def add_months(num_of_months):
         year += 1 * (num_of_months / abs(num_of_months))
 
     return [int(year), int(month)]
-    
+
 class Event(scrapy.Item):
+    # this will be in items.py in final, but for testing purposes I was having trouble importing it so I just pasted it here.
     name = scrapy.Field()
     artist = scrapy.Field()
     venue = scrapy.Field()
@@ -30,34 +31,28 @@ class VenueSpider(scrapy.Spider):
     name = 'venues'
     today = date.today()
 
-    first_concert_month = today.month - 6
-    first_concert_year = today.year
-    if first_concert_month < 1:
-        first_concert_month += 12
-        first_concert_year -= 1
-
     start_urls = []
 
-    for x in range(-3, 4):
+    for x in range(-3, 4): # This range determines how many months into the past and future you are going to scrape.
         year_month = add_months(x)
         start_urls.append('https://first-avenue.com/calendar/all/{year}-{month}'.format(year = year_month[0], month = year_month[1]))
 
     def parse(self, response):
+        # Get the page and saves it as a string.
         page = response.css('body').get()
 
+        # Uses a regex statement to get all the dates
         dates = re.findall("<h3><div class=\"date-repeat-instance\"><span class=\"date-display-single\">([^<]+)([\s\S]+?)</article><!-- /.node -->", page)
         for date in dates:
+            # Uses a regex statement to get all the shows on each date.
             matches = re.findall("\"field-item even\"><a href=\"(/event[^\"]+)\">([^<]+)[\s\S]+?a href=\"/venue[^\"]+\">([^<]+)[\s\S]+?\"date-display-single\">([^<]+)[\s\S]+?even\">([^<]+)", date[1])
         
+            # Saves the show data into all_events
             for match in matches:
                 event = Event(url = 'https://first-avenue.com' + match[0], name = match[1], venue = match[2], time = match[3], ages = match[4], date = date[0])
                 all_events.append(event)
 
-        """next_page = response.css('li.next a::attr("href")').get()
-        if next_page is not None:
-            yield response.follow(next_page, self.parse)"""
-
-
+#all of this is for testing purposes. To be deleted in final code.
 print(all_events)
 
 process = CrawlerProcess()
