3bf698cd297448d3899d622d5944762e90f01629
Flounder <testy@test.com>
2020-04-23 23:24:05 -0500

    Need to set session variable in correct location

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\carson\documents\school\itec 2905-60 software development capstone\project5\lmnop\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
