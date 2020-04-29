7de1edcbf44b9a375b43134ef1571d24387f0208
Flounder <testy@test.com>
2020-04-28 21:10:11 -0500

    Cleaned up testing

--- lmn/tests/test_models.py
+++ lmn/tests/test_models.py
@@ -43,3 +43,32 @@ class TestUser(TestCase):
         user2 = User(username='another_bob', email='Bob@bob.com', first_name='bob', last_name='bob')
         with self.assertRaises(IntegrityError):
             user2.save()
+
+'''
+    Show Table Test
+'''
+class TestShow(self):
+    def test_add_show_successfully(self):
+        artist = Artist(name='The Beatles')
+        artist.save()
+        venue = Venue(name='First Ave', city='Minneapolis', state='MN')
+        venue.save()
+        show = Show(artist=0, venue=0, name='Yellow Submarine', url='https://www.first-ave.com/shows/the-beatles-dec-31st/', time='8:30', ages='18', show_date='12/31/19')
+        show.save()
+
+    def test_duplicate_show(self):
+
+        artist = Artist(name='The Beatles')
+        artist.save()
+        venue = Venue(name='First Ave', city='Minneapolis', state='MN')
+        venue.save()
+        show = Show(artist=0, venue=0, name='Yellow Submarine', url='https://www.first-ave.com/shows/the-beatles-dec-31st/', time='8:30', ages='18', show_date='12/31/19')
+        show.save()
+
+        show2 = Show(artist=0, venue=0, name='Yellow Submarine', url='https://www.first-ave.com/shows/the-beatles-dec-31st/', time='8:30', ages='18', show_date='12/31/19')
+        with self.assertRaises(IntegrityError):
+            show2.save()
+
+
+
+
